525d8b99cb3d98d12ccc197734cf9a83a7e6b757
Thanh Lai <34387230+Thanh-Lai@users.noreply.github.com>
2019-06-22 21:08:33 -0400

    add semi colon

--- src/components/goToPage.js
+++ src/components/goToPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Go to individual page directly
 export default function GoToPage({ goToPage, pages}) {
-    const options = []
+    const options = [];
     for(let i = 0; i < pages; i++) {
       options.push(<option key={i} value={i}>{i + 1}</option>)
     }
